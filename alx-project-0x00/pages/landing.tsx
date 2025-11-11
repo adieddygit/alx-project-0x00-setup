5906ac00e5bb464b28b592527f9d9b86bc737e51
Emmanuel Adi <adiemmanuel10@gmail.com>
2025-11-11 17:55:34 +0000

    Created a Button component with multiple instances used in the landing.tsx file

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -13,17 +13,17 @@ const Landing: React.FC = () => {
         {/* Small Buttons */}
         <Button title="Small Rounded-sm" styles="text-sm px-3 py-1 rounded-sm" />
         <Button title="Small Rounded-md" styles="text-sm px-3 py-1 rounded-md" />
-        <Button title="Small Rounded-full" styles="text-sm px-3 py-1 rounded-full" />
+        <Button title="Small Rounded-full" styles="text-sm px-3 py-1 rounded-lg" />
 
         {/* Medium Buttons */}
         <Button title="Medium Rounded-sm" styles="text-base px-4 py-2 rounded-sm" />
         <Button title="Medium Rounded-md" styles="text-base px-4 py-2 rounded-md" />
-        <Button title="Medium Rounded-full" styles="text-base px-4 py-2 rounded-full" />
+        <Button title="Medium Rounded-full" styles="text-base px-4 py-2 rounded-lg" />
 
         {/* Large Buttons */}
         <Button title="Large Rounded-sm" styles="text-lg px-6 py-3 rounded-sm" />
         <Button title="Large Rounded-md" styles="text-lg px-6 py-3 rounded-md" />
-        <Button title="Large Rounded-full" styles="text-lg px-6 py-3 rounded-full" />
+        <Button title="Large Rounded-full" styles="text-lg px-6 py-3 rounded-lg" />
       </div>
     </div>
   );
