a3aae54b6d5b3e4a63e8803d476b6397bf25d7d5
Emmanuel Adi <adiemmanuel10@gmail.com>
2025-11-11 18:26:29 +0000

    Created a Button component with multiple instances used in the landing.tsx file

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -9,7 +9,7 @@ const Card: React.FC = () => {
 
  return (
     <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
-      <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" loading="eager"   // 👈 Add this line
+      <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" loading="eager" 
         priority />
       <div className="p-2 flex gap-2 mt-2">
         <Pill title="Top Villa" />
